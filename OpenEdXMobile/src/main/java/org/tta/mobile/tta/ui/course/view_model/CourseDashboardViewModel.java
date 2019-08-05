19c92cec87c58ae22ef13375aa301a9e13549f0e
chiragmittal19 <chiragmittal19@gmail.com>
2019-08-05 17:34:15 +0530

    Clear fragments before adding tabs in course

--- OpenEdXMobile/src/main/java/org/tta/mobile/tta/ui/course/view_model/CourseDashboardViewModel.java
+++ OpenEdXMobile/src/main/java/org/tta/mobile/tta/ui/course/view_model/CourseDashboardViewModel.java
@@ -129,6 +129,9 @@ public class CourseDashboardViewModel extends BaseViewModel {
     }
 
     public void setTabs(){
+        fragments.clear();
+        titles.clear();
+
         fragments.add(CourseMaterialTab.newInstance(content, course, rootComponent));
         titles.add(mActivity.getString(R.string.course_material));
 
