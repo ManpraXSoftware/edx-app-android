1c5e35577428b5b6784f3b26fe59cd0e77be569a
chiragmittal19 <chiragmittal19@gmail.com>
2019-12-03 12:11:22 +0530

    Fixed delete feeds broadcast receiver

--- OpenEdXMobile/src/main/java/org/tta/mobile/tta/receiver/DeleteFeedsReceiver.java
+++ OpenEdXMobile/src/main/java/org/tta/mobile/tta/receiver/DeleteFeedsReceiver.java
@@ -1,16 +1,18 @@
 package org.tta.mobile.tta.receiver;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import org.tta.mobile.tta.data.DataManager;
 
-public class DeleteFeedsReceiver extends BroadcastReceiver {
+import roboguice.receiver.RoboBroadcastReceiver;
+
+public class DeleteFeedsReceiver extends RoboBroadcastReceiver {
+
     @Override
-    public void onReceive(Context context, Intent intent) {
+    protected void handleReceive(Context context, Intent intent) {
         if (context != null && intent != null) {
-            DataManager dataManager = DataManager.getInstance(context.getApplicationContext());
+            DataManager dataManager = DataManager.getInstance(context);
             if (intent.getAction() != null && intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
                 dataManager.scheduleDeleteFeeds();
                 return;
